73770bcc772f0bd94c184f7a818c4e9305ba2c8e
makyfj <65879341+makyfj@users.noreply.github.com>
2020-12-04 15:54:50 -0500

    Implementing templates

--- Chapter1/Practice/std::forward_list2.cpp
+++ Chapter1/Practice/std::forward_list2.cpp
@@ -1,4 +1,30 @@
 #include <forward_list>
 #include <iostream>
 
-int main() { return 0; }
+template <typename T>
+
+void print(std::forward_list<T> list) {
+  for (auto ele : list) {
+    std::cout << ele << std::endl;
+  }
+}
+
+int main() {
+
+  std::forward_list<int> list1 = {23, 0, 1, -3, 4, 22};
+
+  list1.sort();
+
+  std::cout << "Low to high"
+            << "\n";
+  print(list1);
+
+  list1.sort(std::greater<int>());
+
+  std::cout << std::endl;
+  std::cout << "High to low"
+            << "\n";
+  print(list1);
+
+  return 0;
+}
