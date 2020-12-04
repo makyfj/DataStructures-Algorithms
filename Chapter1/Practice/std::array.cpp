3b42d9a63918e07313441bc431bfbf0472f0420d
makyfj <65879341+makyfj@users.noreply.github.com>
2020-12-04 08:18:02 -0500

    Deleting arrays

--- Chapter1/Practice/std::array.cpp
+++ Chapter1/Practice/std::array.cpp
@@ -17,17 +17,7 @@ int main() {
   // Passing an arry by value
   std::array<int, 5> yes = {1, 2, 3, 4, 5};
 
-  // Adding a new array
-  std::array<int, 5> no = {100, 100, 100, 100, 100};
-
-  // Adding one more array
-  std::array<int, 5> xx = {99, 99, 99, 99, 99};
-
   print(yes);
-
-  print(no);
-
-  print(xx);
 
   return 0;
 }
