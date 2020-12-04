03ea9a7e648f0f050cf1280768d8307d017640c8
makyfj <65879341+makyfj@users.noreply.github.com>
2020-12-04 16:41:57 -0500

    Compiler error example itfwd += 8 and std::advance(itfwd, -2)

--- Chapter1/Exercise4/Exercise4.cpp
+++ Chapter1/Exercise4/Exercise4.cpp
@@ -24,6 +24,7 @@ int main() {
 
   auto it = vec.begin(); // constant time
 
+  std::cout << "\nVector List" << std::endl;
   std::cout << "Latest winner is: " << *it << std::endl;
 
   it += 8; // constant time
@@ -32,7 +33,25 @@ int main() {
   std::advance(it, -3); // constant time
   std::cout << "Winner before 3 years of that was: " << *it << std::endl;
 
-  // Lets try with forward
+  // Lets try with forward_list
+  std::cout << "\nForward List" << std::endl;
+  std::forward_list<std::string> fwd(vec.begin(), vec.end());
+
+  auto itfwd = fwd.begin();
+
+  std::cout << "Latest winner is: " << *itfwd << std::endl;
+
+  // Time taken is proportional to the numbers of elements
+  std::advance(itfwd, 5);
+
+  std::cout << "Winner before 5 years was: " << *itfwd << std::endl;
+
+  // Going back will result in compile time error as forward_list only
+  // allows us to move towards the end.
+
+  // This will result in a compiler error
+  // std::advance(itfwd, -2);
+  // itfwd += 2; - Compiler error as well
 
   return 0;
 }
