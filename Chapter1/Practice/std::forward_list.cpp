cb610aa8c9af9e9d048bbbe1f8197cc320819af5
makyfj <65879341+makyfj@users.noreply.github.com>
2020-12-04 14:52:08 -0500

    Inserting and removing elements

--- Chapter1/Practice/std::forward_list.cpp
+++ Chapter1/Practice/std::forward_list.cpp
@@ -12,5 +12,39 @@ int main() {
   // Retrieve the first item
   std::cout << *(it) << std::endl;
 
+  fwd_list.insert_after(it, 4);
+
+  fwd_list.insert_after(it, 6);
+
+  // emplace_after and emplace_front better for perfomance
+  fwd_list.emplace_after(it, 10);
+
+  fwd_list.emplace_front(100);
+
+  std::cout << "Forward_List: ";
+  for (auto ele : fwd_list) {
+    std::cout << ele << ", ";
+  }
+
+  std::cout << std::endl;
+
+  // Removing elements from a forward_list
+
+  // pop first element
+  fwd_list.pop_front();
+
+  auto it2 = fwd_list.begin();
+
+  // erase element after first element
+  fwd_list.erase_after(it2);
+
+  // erase every element except first one
+  fwd_list.erase_after(it2, fwd_list.end());
+
+  std::cout << "Forward_List: ";
+  for (auto ele : fwd_list) {
+    std::cout << ele << ", ";
+  }
+
   return 0;
 }
