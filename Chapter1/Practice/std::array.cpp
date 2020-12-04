64e90169825cf0e990e433de5c70221e403e7396
makyfj <65879341+makyfj@users.noreply.github.com>
2020-12-04 07:51:48 -0500

    Chapter 1: List, Stacks, and Queues

---
+++ Chapter1/Practice/std::array.cpp
@@ -0,0 +1,29 @@
+#include <array>
+#include <cstddef>
+#include <iostream>
+
+void print(std::array<int, 5> arr);
+
+int main() {
+
+  std::array<int, 10> arr1 = {10, 8, 2, 33, 44, 55, 77, 55, 11, 11};
+
+  for (unsigned int i = 0; i < arr1.size(); i++) {
+    std::cout << "Element " << i << " : " << arr1[i] << std::endl;
+  }
+
+  std::cout << arr1.at(8) << std::endl;
+
+  // Passing an arry by value
+  std::array<int, 5> yes = {1, 2, 3, 4, 5};
+
+  print(yes);
+
+  return 0;
+}
+
+void print(std::array<int, 5> arr) {
+  for (auto ele : arr) {
+    std::cout << "Print: " << ele << std::endl;
+  }
+}
