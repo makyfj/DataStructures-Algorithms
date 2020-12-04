3f48b6d123e9edce67928bb92cab26fffb2a7d02
makyfj <65879341+makyfj@users.noreply.github.com>
2020-12-04 16:15:35 -0500

    using unique with a lambda function

--- Chapter1/Practice/std::forward_list2.cpp
+++ Chapter1/Practice/std::forward_list2.cpp
@@ -5,7 +5,7 @@ template <typename T>
 
 void print(std::forward_list<T> list) {
   for (auto ele : list) {
-    std::cout << ele << std::endl;
+    std::cout << ele << ", ";
   }
 }
 
@@ -25,6 +25,45 @@ int main() {
   std::cout << "High to low"
             << "\n";
   print(list1);
+
+  std::cout << std::endl;
+
+  // Using reverse, sort and unique
+  std::forward_list<int> list2 = {2, 53, 1, 0, 4, 10};
+
+  std::cout << "Original list" << std::endl;
+  print(list2);
+
+  list2.reverse();
+
+  std::cout << "\nReverse list" << std::endl;
+  print(list2);
+
+  list2 = {0, 1, 0, 1, -1, 10, 5, 10, 5, 0};
+
+  list2.sort();
+
+  std::cout << "\nSorting list" << std::endl;
+  print(list2);
+
+  list2.unique();
+
+  std::cout << "\nUnique number in the list: " << std::endl;
+  print(list2);
+
+  // Another example
+  list2 = {0, 1, 0, 1, -1, 10, 5, 10, 5, 0};
+
+  list2.sort();
+
+  std::cout << "\nSorting list" << std::endl;
+  print(list2);
+
+  // Remove elements if they are not greater than the
+  // previously valid element by at least 2
+  list2.unique([](int a, int b) { return (b - a) < 2; });
+  std::cout << "\nUnique list" << std::endl;
+  print(list2);
 
   return 0;
 }
